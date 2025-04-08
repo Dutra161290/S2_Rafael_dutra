ae010dcb723befce03870cc5f43b667f4eec6d06
Dutra161290 <wandersondutra465@gmail.com>
2025-04-08 09:06:05 -0300

    cavalo1
    isso é a continuacao da atividade esse é o primeiro branch

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # S2_Rafael_dutra
- atividade avaliativa marlon
+   Atividade Avaliativa - 08/04/25 Este repositório contém a atividade avaliativa solicitada pelo professor Marlon. 
+   A tarefa foi realizada no dia 08 de abril de 2025 e tem como objetivo praticar conceitos de programação em Python. 
+   A atividade será utilizada para avaliação de desempenho e compreensão dos conteúdos abordados nas aulas. 
+   O código contém um exercício com um erro intencional para ser corrigido. 
+   A correção desse erro será fundamental para a conclusão bem-sucedida da tarefa.
--- hortifruit/loja.py
+++ hortifruit/loja.py
@@ -3,12 +3,12 @@ class LojaAlimentos:
         self.produtos = {
             "Arroz": 5.00,
             "Feijão": 4.50,
-            "cavalo": 3.00,
-            "ninja roberto": 6.20
+            "Macarrão": 3.00,
+            "Óleo": 6.20
         }
 
     def mostrar_produtos(self):
-        print("cavalos disponíveis na loja:")
+        print("Produtos disponíveis na loja:")
         for produto, preco in self.produtos:
             print(f"{produto}: R${preco}")
 
@@ -17,11 +17,11 @@ class LojaAlimentos:
         for item in items:
             total += self.produtos[item]
         return total
-
+    
 loja = LojaAlimentos()
 
 loja.mostrar_produtos()
 
 itens_comprados = ["Arroz", "Feijão", "Óleo"]
 total = loja.calcular_total(itens_comprados)
-print(f"Total do roberto seu nome : R${total}")
+print(f"Total da compra: R${total}")
